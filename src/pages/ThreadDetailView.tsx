d909b9dea0e2dab78da12c2b9f7d49ae766bc344
natsuki.i <natsuki.iida0511@gmail.com>
2023-09-09 21:49:38 +0200

    Added a Comment component

--- src/pages/ThreadDetailView.tsx
+++ src/pages/ThreadDetailView.tsx
@@ -1,7 +1,11 @@
 import Comment from '../components/Comment';
 import { useParams } from 'react-router-dom';
+import { createContext, useState } from 'react';
+
+const MyContext = createContext('value');
 
 const ThreadDetailView = () => {
+  const [message, setMessage] = useState("Hello from parent");
   const savedPosts = JSON.parse(localStorage.getItem('posts') || '[]');
   const { id } = useParams()
   // let post: any[] = savedPosts;
@@ -10,19 +14,18 @@ const ThreadDetailView = () => {
     <div className="card">
         <div className="a-post">
           {/* ID: {id} */}
+          
         {savedPosts.map(function(post:any){
          if(id == post.id){
           return(
             <div key={post.id}>
               <p>Title: {post.title}</p>
               <p>Content: {post.content}</p>
+              <Comment post={post} />
             </div>
           )
          } else (<div>There are no applicable thereds. </div>)
         })}
-        </div>
-        <div>
-          <Comment />
         </div>
     </div>
   );

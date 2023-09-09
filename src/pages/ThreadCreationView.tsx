42d7e5da61bfb509f4e6694be3811eab304d43c5
natsuki.i <natsuki.iida0511@gmail.com>
2023-09-09 12:40:04 +0200

    able to create a post here

--- src/pages/ThreadCreationView.tsx
+++ src/pages/ThreadCreationView.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-// import Post from '../components/Post'
+import Post from '../components/Post'
+
 //Skapa en kommentar
 const ThreadCreationView = () => {
   return (
     <div>
-
+      <Post />
     </div>
   )
 }

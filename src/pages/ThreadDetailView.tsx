9f5932c1fafa0227b3b3fc521a1474e90575fd77
natsuki.i <natsuki.iida0511@gmail.com>
2023-09-09 12:37:30 +0200

    added the code for to show ThreadDetailView

--- src/pages/ThreadDetailView.tsx
+++ src/pages/ThreadDetailView.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-//Visa detaljer av specifik tråd
-const ThreadDetailView = () => {
-  return (
-    <div>ThreadDetailView</div>
-  )
-}
+import Comment from '../components/Comment';
+import { useParams } from 'react-router-dom';
 
-export default ThreadDetailView
+const ThreadDetailView = () => {
+  const savedPosts = JSON.parse(localStorage.getItem('posts') || '[]');
+  const { id } = useParams()
+  // let post: any[] = savedPosts;
+
+  return (
+    <div className="card">
+        <div className="a-post">
+          {/* ID: {id} */}
+        {savedPosts.map(function(post:any){
+         if(id == post.id){
+          return(
+            <div key={post.id}>
+              <p>Title: {post.title}</p>
+              <p>Content: {post.content}</p>
+            </div>
+          )
+         } else (<div>There are no applicable thereds. </div>)
+        })}
+        </div>
+        <div>
+          <Comment />
+        </div>
+    </div>
+  );
+};
+
+export default ThreadDetailView;
